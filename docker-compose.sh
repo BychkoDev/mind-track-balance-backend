d717b0a2ef66261983ba3d96137f4b1e86796ba8
BychkoDev <bychko.job@gmail.com>
2026-03-15 21:21:24 +0200

    feat: Add database initialization script and clean up commented services in docker-compose-dev.yml while updating .gitignore.

--- docker-compose.sh
+++
@@ -1,6 +0,0 @@
-docker compose -f docker-compose-dev.yml --env-file .env.dev up -d
-
-docker-compose -f docker-compose-dev.yml --env-file .env.dev up postgres
-docker-compose -f docker-compose-dev.yml --env-file .env.dev up redis
-#docker compose -f docker-compose-prod.yml --env-file .env.prod up -d
-
